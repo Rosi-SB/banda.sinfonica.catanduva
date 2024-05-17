5485436d9c71afeb7d82851502e3345c0275d8cd
Rosi-SB <122313206+Rosi-SB@users.noreply.github.com>
2024-05-17 14:46:31 -0300

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,9 +11,9 @@ function Contact(){
         <>
             <Header />
             <Container>
-                <section className={style.contact}>
+                <section >
                     <Title>IMPRENSA</Title>
-                    <div>
+                    <div className={style.contact}>
                         <img src="../images/Photos/IMG-2021-18.jpg" alt="foto da banda"/>
                         <p>Para entrevistas ou dúvidas relacionadas à imprensa, entre em contato diretamente pelo e-mail projetosinfonica@gmail.com ou pelo número
                             (11) 11111-1111, com o assessor de comunicação João Paulo.</p>
